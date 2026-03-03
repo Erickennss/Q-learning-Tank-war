81b20a0cafb5598180c029a364cc8535f8f19cf6
Erickennss <138311751+Erickennss@users.noreply.github.com>
2026-03-03 18:46:14 +0800

    Add files via upload

---
+++ TankGame/explosion.py
@@ -0,0 +1,26 @@
+# explosion.py
+import pygame
+import os
+
+class Explosion:
+    def __init__(self, tank):
+        self.rect = tank.rect
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.images = [
+            pygame.image.load(os.path.join(BASE_DIR, "Resource", "img", "fire", "blast1.gif")),
+            pygame.image.load(os.path.join(BASE_DIR, "Resource", "img", "fire", "blast2.gif")),
+            pygame.image.load(os.path.join(BASE_DIR, "Resource", "img", "fire", "blast3.gif")),
+            pygame.image.load(os.path.join(BASE_DIR, "Resource", "img", "fire", "blast4.gif"))
+        ]
+        self.index = 0
+        self.image = self.images[self.index]
+        self.is_live = True
+
+    def displayExplosion(self, window):
+        if self.index < len(self.images):
+            self.image = self.images[self.index]
+            self.index += 1
+            window.blit(self.image, self.rect)
+        else:
+            self.is_live = False
+            self.index = 0
